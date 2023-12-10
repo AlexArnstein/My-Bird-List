899cd402253a27910e3f3a206f15969a1ce4410d
Alexander Arnstein <106594172+AlexArnstein@users.noreply.github.com>
2023-12-10 18:31:36 +0000

    Nodejs now connecting with MySQL and filled birds table

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const mysql = require('mysql2');
 const express = require('express');
 
+const fs = require('fs');
+const { parse } = require('csv-parse');
+
 // Create connection
 const connection = mysql.createConnection({
 	host	 : '127.0.0.1',
@@ -14,7 +17,8 @@ connection.connect((err) => {
 		console.log('Cannot connect to MySQL');
 		console.log(err);
 	} else {
-		console.log("MySQL connected");
+		console.log("MySQL database connected");
+		//connection.query("SELECT * FROM birds WHERE species = 'King Parrot';", (err, result) => {console.log(result)});
 		// connection.query("CREATE DATABASE mydb", function (err, result) {
 		// 	if (err) {
 		// 		console.log("Cannot create database");
@@ -31,24 +35,57 @@ connection.connect((err) => {
 		// 		console.log("Table created");
 		// 	}
 		// })
-		connection.query("INSERT INTO birds (species, bird_group) VALUES ('King Parrot', 'Parrots');", function (err, result) {
-			if (err) {
-				console.log("Cannot insert");
-				console.log(err);
-			} else {
-				console.log('1 row inserted');
-			}
-		})
-		connection.query("SELECT * FROM birds;", function (err, result) {
-			if (err) {
-				console.log("Cannot select table");
-				console.log(err);
-			} else {
-				console.log(result);
-			}
-		})
-	}
-})
+		// connection.query("INSERT INTO birds (species, bird_group) VALUES ('King Parrot', 'Parrots');", function (err, result) {
+		// 	if (err) {
+		// 		console.log("Cannot insert");
+		// 		console.log(err);
+		// 	} else {
+		// 		console.log('1 row inserted');
+		// 	}
+		// })
+		// connection.query("SELECT * FROM birds;", function (err, result) {
+		// 	if (err) {
+		// 		console.log("Cannot select table");
+		// 		console.log(err);
+		// 	} else {
+		// 		console.log(result);
+		// 	}
+		// })
+		// const data = [];
+
+		// fs.createReadStream('C:\\Users\\alexa\\OneDrive\\Documents\\04 Bird List\\data\\bird_data.csv')
+		// .pipe(parse({ delimiter: '\r\n', from_line: 1}))
+		// .on('data', function (row) {
+		// 	row = row[0].split(',');
+		// 	data.push(row);
+		// })
+		// .on('error', function (error) {
+		// 	console.log(error.message);
+		// })	
+		// .on('end', function () {
+		// 	console.log('Data loaded');
+		// 	//connection.query("ALTER TABLE birds ADD Favourites VARCHAR(5);", (err, result) => {console.log(result)});
+		// 	connection.query('DELETE FROM birds;');
+
+		// 	for (i = 3; i < data.length; i++) {
+		// 		connection.query(`INSERT INTO birds (species, bird_group, United_Kingdom, Vietnam, Australia, New_Zealand, Indonesia, Malaysia, Thailand, Spain, Comments, Favourites, Image_link) VALUES ("${data[i][1]}", "${data[i][2]}", 
+		// 			"${data[i][3]}", "${data[i][4]}", "${data[i][5]}", "${data[i][6]}", "${data[i][7]}", "${data[i][8]}",
+		// 			"${data[i][9]}", "${data[i][10]}", "${data[i][11]}", "${data[i][12]}", "${data[i][13]}"
+		// 			);`, function (err, result) {
+		// 		if (err) {
+		// 			console.log("Cannot insert");
+		// 			console.log(err);
+		// 		}
+		// 		});
+		// 	};
+		// 	console.log('Data inserted into SQL table');
+		//	connection.query("SELECT * FROM birds WHERE species = 'Pacific Swift';", (err, result) => {console.log(result)});
+		//});
+
+
+
+	};
+});
 
 const app = express();
 
